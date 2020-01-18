6c61d77ad42381a5bf5be95a4d4ff69a07f16fa9
Manel <mviel@florida-uni.es>
2020-01-18 20:12:41 +0100

    Drawer

--- App.js
+++ App.js
@@ -11,14 +11,40 @@ import 'react-native-gesture-handler';
 import {PrimeraScreen} from './screens/PrimeraScreen';
 import {SegonaScreen} from './screens/SegonaScreen';
 import {TerceraScreen} from './screens/TerceraScreen';
-import {StyleSheet, View, Text, Button} from 'react-native';
+import {StyleSheet,Image,  View, Text, Button} from 'react-native';
 
 // 1r Importem els components que necessitem per a la navegació
+import {createDrawerNavigator} from 'react-navigation-drawer';
 import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
+/*
+  // UTILITZAT PER A LA NAVEGACIÓ WEB
+  import {createStackNavigator} from 'react-navigation-stack';
+*/
+//Opcional creem un objecte amb icones
+const icons = {
+  home: require("./images/home.png"),
+}
 
-//2n Creem el Navigation Stack
-export const NavegadorDeLApp = createStackNavigator(
+//2n Creem el Drawer Navigation
+export const ElMeuDrawer = createDrawerNavigator(
+  {
+    Primera: {screen: PrimeraScreen,
+    navigationOptions: ({navigation})=>({
+      title:'Primera Pantalla Manel',
+      drawerIcon: ({item})=> (
+        <Image source={icons.home} style={{width:20, height:20}} />
+      )
+    })},
+    Segona: {screen: SegonaScreen},
+    Tercera: {screen: TerceraScreen},
+  },
+  {
+    // Altres opcions del Drawer Navigator
+    title: 'Menu de Manel',
+  }
+);
+/*
+ export const NavegadorDeLApp = createStackNavigator(
   {
     Primera: {screen: PrimeraScreen},
     Segona: {screen: SegonaScreen},
@@ -26,9 +52,10 @@ export const NavegadorDeLApp = createStackNavigator(
   },
   {initialRoutename: 'Primera',},
 );
+*/
 
-//3r creem el contenidor de l'app per a suportar el botó back
-const AppContainer = createAppContainer(NavegadorDeLApp);
+//3r creem el contenidor de l'app per a suportar el drawerNavigator
+const AppContainer = createAppContainer(ElMeuDrawer);
 
 export default class App extends React.Component {
   render() {
